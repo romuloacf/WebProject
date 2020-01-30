d05cb13bec57ac19235bce2c9e5d802e9df80809
Romulo <romulo.acf@gmail.com>
2020-01-30 23:20:30 +0000

    Adaptacao para MySQL and Migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -10,6 +10,10 @@ namespace SalesWebMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public Department()
+        {
+        }
+
         public Department(int id, string name)
         {
             Id = id;
