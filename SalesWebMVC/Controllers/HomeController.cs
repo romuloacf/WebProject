d7d3abe9a6a302aaf6a79bf61cbf3649f3ab0dc5
Romulo <romulo.acf@gmail.com>
2020-01-30 01:57:09 +0000

    First controller and Razor pages test

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web Learning Version from C#";
+            ViewData["Developer"] = "Romulo Augusto";
 
             return View();
         }
