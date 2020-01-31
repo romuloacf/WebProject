d1dbba374c48e667b689ba266dc4ecd5ca76fd47
Romulo <romulo.acf@gmail.com>
2020-01-31 22:38:17 +0000

    other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }  = new List<Seller>();
 
         public Department()
         {
         }
-
-        public Department(int id, string name)
+        public Department(int id, string name) //Nao criar construtor para coleções, apenas o generico.
         {
             Id = id;
             Name = name;
         }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        //pegar lista de todos os vendededores e somar dentro desse intervalo de data
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+        
+
     }
 }
